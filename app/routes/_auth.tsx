b04d34ed0aa3dd893152308d96be2a029501f989
Rinkesh-modi <rinkeshmodi012@gmail.com>
2025-07-12 11:26:31 +0530

    new api integrated

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "@remix-run/react";
 import React from "react";
 
 const AuthRoute = () => {
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Outlet />
